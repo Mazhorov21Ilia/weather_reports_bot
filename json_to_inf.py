27416dcba00e6df418bb72ab327757e34dfe876f
ilia mazhorov <ilia.liash@yandex.ru>
2024-11-14 18:40:27 +0300

    Second version with more understanding message

--- json_to_inf.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open('forecast.json', 'r') as file:
-    data = json.load(file)
-    for i in data["forecast"]["forecastday"]:
-        print(f"{i["date"]} - {i["day"]["condition"]["text"]}")
--- main.py
+++ main.py
@@ -2,30 +2,29 @@ from telebot import TeleBot
 import json
 
 token = "7891467095:AAERGih85-8a7QbJ5gqaXerbDtamkGpaGlI"
-
 bot = TeleBot(token=token)
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Напиши дату в ближайшие 14 дней в формате гггг-мм-дд")
+	bot.reply_to(message, "Напишите дату в ближайшие 14 дней в формате гггг-мм-дд")
 
 def search_weather(date):
 	with open('forecast.json', 'r') as file:
 		data = json.load(file)
 		for i in data["forecast"]["forecastday"]:
 			if date == i["date"]:
-				return f"Что будет ночью: {i["astro"]}"
+				return f"Погода в Москве {i["date"]} {i["day"]["condition"]["text"]}:\nТемпература {i["day"]["avgtemp_c"]}C\nШанс дождя {i["day"]["daily_chance_of_rain"]}%\nШанс снега {i["day"]["daily_chance_of_snow"]}%"
 			# print(f"{i["date"]} - {i["day"]["condition"]["text"]}")
 
 @bot.message_handler()
 def zaglushka(message):
 	text = message.text
-	print(text)
 	res = search_weather(text)
 	bot.send_message(message.chat.id, res)
 	
 	
 
 
-
-bot.infinity_polling()
+if __name__ == "__main__":
+	bot.infinity_polling()
